7dd92b285532990625d03a5772ebed0b86ab3012
Agustin Marini <agustin@MacBook-Air-de-Agustin.local>
2025-11-09 16:32:11 +0200

    push

---
+++ week1/ExercisesXP/Exercise1/Exercise1.py
@@ -0,0 +1,61 @@
+#Exercise 1 
+#print('\nHello world'  * 4 )
+
+#Exercise2 
+math_calculate = (99 * 99 * 99) * 8
+#print (math_calculate) 
+
+#Exercise 3 
+#print(5 < 3)
+#print (3 == 3)
+#print (3 == "3")
+#print("Hello" == "hello")
+#print ("3" > 3) #Esto no entiendo por que da error. 
+#false 
+#true 
+#false
+#false 
+#false 
+
+#Exercise 4 
+computer_brand = 'mac'
+#print(f"I have a" ,computer_brand , "computer.")
+
+#Exercise 5
+name = 'Agustin'
+age = '31'
+shoe_size = '42'
+info = 'Im ' + name + ' and im ' + age + ' and my shoes are ' + shoe_size
+print(info)
+
+#Exercise 6 
+a = 4
+b = 6
+if a > b:
+    print('Hello world')
+else :
+    print ('Is not bigger')
+
+#Exercise 7 
+#number = int(input('put a number'))
+#calculate = number % 2
+#if calculate == 0 :
+##    print('number par')
+#else: 
+#    print('number impar')
+
+#Exercise 8 
+name = str(input('What is your name?'))
+mi_name = 'Agustin' 
+if name == mi_name:
+    print('Amazing, we have the same beautiful name')
+else :
+    print('your name is awfull')
+
+#Exercise 9 
+user_age = int(input('Put your height'))
+if user_age > 145 :
+    print('you can ride the bicycle')
+else :
+    print ('go walking, you cant ride')
+    
