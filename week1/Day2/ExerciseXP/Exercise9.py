a968dd2e899c6ba7b2df76e677340e1dfca71fdb
Agustin Marini <agustin@MacBook-Air-de-Agustin.local>
2025-11-13 11:02:35 +0200

    Exercise 9

--- week1/Day2/ExerciseXP/Exercise9.py
+++ week1/Day2/ExerciseXP/Exercise9.py
@@ -1,21 +1,31 @@
-total_family = input('how many members are')
-age_family = input('age of each person') #esto es para pedir la edad de cada familiar
+total_family = int(input('how many members are'))
 ages = [] #hago una lista vacia para guardar las edades de cada familiar
 askings = 0 #para preguntar la edad de cada uno, 
 price = 0 #aca hago una variable para calcular el precio de la entrada 
 
 #index = 0 THIS I DONT KNOW WHY I DID IT 
-while askings < int(total_family): #si la cantidad de familiares es 5, debo preguntar 5 edades. 
-    ages.append(age_family) #aca guardo en ages, las edades. 
-    age_family = input(age_family) #aca vuelvo a pedir la edad 
+while askings <= total_family - 1:
+    age_family = int(input('age of each person')) #esto es para pedir la edad de cada familiar #mientras que el total de veces preguntados sea distinto o igual que la cantidad de familiares.. hacer 
+    ages.append(age_family)  #Agrego la edad a la lista ages 
+    askings += 1 #incremento en uno las veces preguntadas para que siga hasta que se cumple el while...
 
+print(ages)   
+
+#ESTO NO SE PARA QUE LO HICE NI SE SI ESTA BIEN 
+#while askings < int(total_family): #si la cantidad de familiares es 5, debo preguntar 5 edades. 
+    #ages.append(age_family) #aca guardo en ages, las edades. 
     #here i make a loop to recorrer the ages
-    for edades in ages: #aca recorro las edades que tiene cada miembro 
+
+
+for age in ages: #aca recorro las edades que tiene cada miembro 
         #edades = int(edades)
-        if int(edades) <= 3: 
-            price += 0
-        elif int(edades) <= 12: 
+    if age <= 3: 
+        price += 0
+    elif age <= 12: 
             price += 10
-        else:
-            price += 15
+    else:
+        price += 15
+    
+
+
 print('are ' , total_family , 'people and the cost is ' , price)
