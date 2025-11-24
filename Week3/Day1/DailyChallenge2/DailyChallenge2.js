1ddf0a8e86774a5b0e2ddc61ba7c3df2eb6e6be2
Agustin Marini <agustin@MacBook-Air-de-Agustin.local>
2025-11-24 12:33:40 +0200

    refresh

---
+++ Week3/Day1/DailyChallenge2/DailyChallenge2.js
@@ -0,0 +1,14 @@
+row = '';
+for (let i = 0; i < 6; i++) {
+        // i = '*';
+        row += '* ';
+        console.log(row);
+    }
+//Solution 2 
+for (let y = 0; y < 1; y++){
+    r = '';
+    for (let j = 0; j < 6; j++){
+        r += '* ';
+        console.log(r);
+    }
+}
--- Week3/Day1/ExerciseXp/exercise1.js
+++ Week3/Day1/ExerciseXp/exercise1.js
@@ -3,34 +3,41 @@ const people = ["Greg", "Mary", "Devon", "James"];
 // (arrays y objetos pueden cambiar). No se puede declarar dos veces en el mismo scope.
 
 // ---- PARTE1 ----
-//Write code to remove “Greg” from the people array.
-// people.shift()
-// console.log(people)
+// Write code to remove “Greg” from the people array.
+people.shift()
+console.log(people)
 
-//Write code to replace “James” to “Jason”.
-// people.splice(3, 1, 'Jason')
-// console.log(people)
+//----- PARTE 2 ------
+// Write code to replace “James” to “Jason”.
+people.splice(3, 1, 'Jason')
+console.log(people)
 
-//Write code to add your name to the end of the people array.
-// people.push('Agustin');
-// console.log(people);
+//----- PARTE 3 -------
+// Write code to add your name to the end of the people array.
+people.push('Agustin');
+console.log(people);
 
-//Write code that console.logs Mary’s index. take a look at the indexOf method on Google.
-// console.log(people.indexOf('Mary'));
+// ------ PARTE 4 ------------
+// Write code that console.logs Mary’s index. take a look at the indexOf method on Google.
+console.log(people.indexOf('Mary'));
 
+// --------- PARTE 5 ----------
 
 //Write code to make a copy of the people array using the slice method.
 let peopleSlice = people.slice(0, 1).concat(people.slice(2, 4)) //slice(0,1) toma el elemento 0, pero no el 1. concat concatena la cadena, y slice(2,4) toma el elemento 2, 3, y 4 que es el ultimo lo borra.
 // console.log()
-// console.log(peopleSlice)
+console.log(peopleSlice)
 
+// --------PARTE 6 ----- 
 //Write code that gives the index of “Foo”. Why does it return -1 ?
-// console.log(people.indexOf('Foo'))
+console.log(people.indexOf('Foo'))
 //will return -1 because is not in the array
 
+
+// -------- PARTE 7 ------- 
 //Create a variable called last which value is the last element of the array.
-// let lastVariable = people.slice(-1)
-// console.log(lastVariable) 
+let lastVariable = people.slice(-1)
+console.log(lastVariable) 
 //length is showing how many elements there're in the array, and the last index element is the quantity
 
 
@@ -45,6 +52,7 @@ for (let i = 0; i < people.length; i++) {
 //Using a loop, iterate through the people array and exit the loop after you console.log “Devon” .
 let j = 0;
 while (people[j] != 'Devon') {
-    console.log('Name in the index ' + j + 'is  --> ' +       people[j]) 
+    console.log('Name in the index ' + j + 'is  --> ' + people[j]) 
     j++;
+    break;
 }
