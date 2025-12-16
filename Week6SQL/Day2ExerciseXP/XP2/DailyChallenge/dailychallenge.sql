4d43d6a46fb539347376bb07173b52b2509ae222
Agusin <agumarini94@gmail.com>
2025-12-16 09:36:33 +0200

    dale

--- Week6SQL/Day2ExerciseXP/XP2/DailyChallenge/dailychallenge.sql
+++ Week6SQL/Day2ExerciseXP/XP2/DailyChallenge/dailychallenge.sql
@@ -32,13 +32,19 @@ Table2 – SecondTab
 ID
 5
 NULL
+SELECT COUNT(*) 
+    FROM FirstTab AS ft WHERE ft.id NOT IN ( SELECT id FROM SecondTab WHERE id IS NULL )
+-- //1 =   0 , because when is comparing with null, any element satisfied the condition. SQL can not be secure which element is diferent of null.
+    SELECT COUNT(*) 
+    FROM FirstTab AS ft WHERE ft.id NOT IN ( SELECT id FROM SecondTab WHERE id = 5 )
+-- //2 = 2 because theres two elements of the first table that are not in the second table 
 
-//1 =   0 , because when is comparing with null, any element satisfied the condition. SQL can not be secure which element is diferent of null.
+SELECT COUNT(*) 
+    FROM FirstTab AS ft WHERE ft.id NOT IN ( SELECT id FROM SecondTab )
+-- //3 = 0 // because when not in compare with an element null, any element satisfied the condition. 
 
-//2 = 2 because theres two elements of the first table that are not in the second table 
-
-//3 = 0 // because when not in compare with an element null, any element satisfied the condition. 
-
-//4 = 2 Because it counts how many rows from the first table are not present in the second table, considering only the non-NULL IDs.
-In the second table there is only one non-NULL value, which is 5.
-From the first table, only the rows with IDs 6 and 7 are not in the second table, since NULL is not counted.
+SELECT COUNT(*) 
+    FROM FirstTab AS ft WHERE ft.id NOT IN ( SELECT id FROM SecondTab WHERE id IS NOT NULL )
+-- //4 = 2 Because it counts how many rows from the first table are not present in the second table, considering only the non-NULL IDs.
+-- In the second table there is only one non-NULL value, which is 5.
+-- From the first table, only the rows with IDs 6 and 7 are not in the second table, since NULL is not counted.
